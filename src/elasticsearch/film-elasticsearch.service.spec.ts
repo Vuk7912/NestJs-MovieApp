43c1514911c64bd66cc6d7e8bfb30991921c372c
chikaloo <chikaloo@users.noreply.github.com>
2025-06-17 23:20:36 +0000

    Add vi import to test file

--- src/elasticsearch/film-elasticsearch.service.spec.ts
+++ src/elasticsearch/film-elasticsearch.service.spec.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, beforeEach } from 'vitest';
+import { describe, it, expect, beforeEach, vi } from 'vitest';
 import { Test, TestingModule } from '@nestjs/testing';
 import { ElasticsearchService } from '@nestjs/elasticsearch';
 import { FilmElasticsearchServiceImpl } from './film-elasticsearch.service';

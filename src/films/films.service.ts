a1d6542ff15c3887ba181841a9fb2f1e2228ea06
ranjhayhero <ranjhayhero@users.noreply.github.com>
2025-06-18 00:30:41 +0000

    Add initial FilmsService with updateFilm method

---
+++ src/films/films.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { ElasticsearchService } from '@nestjs/elasticsearch';
+
+@Injectable()
+export class FilmsService {
+  constructor(private readonly elasticsearchService: ElasticsearchService) {}
+
+  async updateFilm(id: string, updateFilmDto: any) {
+    try {
+      const response = await this.elasticsearchService.update({
+        index: 'films',
+        id,
+        body: {
+          doc: updateFilmDto
+        }
+      });
+      return response.body;
+    } catch (error) {
+      throw new Error(`Failed to update film: ${error.message}`);
+    }
+  }
+}

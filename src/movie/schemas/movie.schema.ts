a5c1f1bdbd95ad00d42793adb158b4dfedd18311
CryptoSCK <CryptoSCK@users.noreply.github.com>
2025-06-18 00:30:21 +0000

    Update Movie schema to match test requirements

--- src/movie/schemas/movie.schema.ts
+++ src/movie/schemas/movie.schema.ts
@@ -6,32 +6,22 @@ export type MovieDocument = HydratedDocument<Movie>;
 @Schema()
 export class Movie {
   @Prop({ required: true })
-  name: string;
+  _id: string;
+
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ type: [String], default: [] })
+  genre: string[];
 
   @Prop()
   description: string;
 
   @Prop()
-  release_date: string;
-
-  // For the sake of this project, we will assume we only have ticket prices in PKR
-  @Prop()
-  ticket_price: number;
+  year: number;
 
   @Prop()
-  country: string;
-
-  @Prop()
-  genre: string;
-
-  @Prop()
-  photo_uri: string;
-
-  @Prop({ default: [] })
-  ratings: { userId: string; rating: number }[]; // Array of objects to store user ratings
-
-  @Prop({ default: [] })
-  comments: { userId: string; text: string }[]; // Array of objects to store user comments
+  director: string;
 }
 
 export const MovieSchema = SchemaFactory.createForClass(Movie);
